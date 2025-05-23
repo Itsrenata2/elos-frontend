6f9ef896be1b4c011914d2aa6f20008201384139
itsrenata2 <renatasilva2222@hotmail.com>
2025-05-22 21:26:16 -0300

    configuração do ambiente do projeto, desenvolvimento da tela de início, criação dos arquivos das telas de login e cadastro

--- postcss.config.js
+++ postcss.config.js
@@ -1,21 +1,6 @@
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        jost: ['"Jost"', "sans-serif"],
-        nunito: ['"Nunito Sans"', "sans-serif"],
-      },
-      colors: {
-        zinc: {
-          950: "#09090b",
-          900: "#18181b",
-          800: "#27272a",
-          200: "#e4e4e7",
-          50: "#fafafa",
-        },
-      },
-    },
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
   },
-  plugins: [],
 };
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,122 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>Elos</h1>;
+  const accordions = [
+    {
+      title: "Cartilhas para download",
+      description: [
+        `<a href="https://www.anamatra.org.br/images/LGBTQIA/CARTILHAS/Cartilha_Comiss%C3%A3o_LGBTQIAPN.pdf" target="_blank" rel="noopener noreferrer" class="text-zinc-600 underline">
+      Cartilha de Direitos da Comunidade LGBTQIAPN+
+    </a>`,
+      ],
+    },
+    {
+      title: "Direitos garantidos por lei",
+      description: [
+        "Direito ao nome social e identidade de gênero",
+        "Criminalização da LGBTQIA+fobia (STF, 2019)",
+        "Direito ao uso do banheiro de acordo com a identidade de gênero",
+        "Acesso à saúde e educação sem discriminação",
+      ],
+    },
+    {
+      title: "Canais oficiais de denúncia",
+      description: [
+        "Disque 100 (Direitos Humanos)",
+        "Ouvidorias de Direitos Humanos estaduais",
+        "Delegacia Online",
+        "Aplicativo Direitos Humanos Brasil",
+      ],
+    },
+    {
+      title: "Contatos úteis (Defensoria, ONGs, etc.)",
+      description: [
+        "Defensoria Pública do Estado: (xx) xxxx-xxxx",
+        "ONGs de apoio: Grupo Dignidade, ABGLT, Transgrupo Marcela Prado",
+        "Centros de referência em direitos humanos",
+        "Atendimento psicológico gratuito: verifique unidades do SUS",
+      ],
+    },
+  ];
+
+  return (
+    <div className="min-h-screen bg-zinc-200 flex flex-col items-center justify-center py-8 font-jost">
+      <div className="absolute top-4 w-full px-8 flex items-center justify-between text-zinc-800">
+        <div>
+          <img src="/logo-elos.svg" alt="Elos Logo" className="w-24 h-auto" />
+        </div>
+        <div className="font-bold flex gap-6 items-center">
+          <Link href="/login" className="hover:underline">
+            login
+          </Link>
+          <Link href="/cadastro" className="hover:underline">
+            cadastro
+          </Link>
+        </div>
+      </div>
+      <div className="pt-20">
+        <h1 className="text-4xl font-black mb-4 text-center text-zinc-900 uppercase">
+          Sua voz importa. Sua segurança é prioridade.
+        </h1>
+        <p className="text-lg font-semibold text-center max-w-2xl mb-8 px-4 mx-auto text-zinc-900">
+          Nossa plataforma existe para acolher, registrar e acompanhar denúncias
+          de LGBTQIA+fobia, oferecendo também apoio jurídico e psicológico
+          gratuito.
+        </p>
+      </div>
+      <hr className="border-zinc-800 w-2/4 mb-8" />
+      <h2 className="text-2xl mb-6 text-zinc-900 uppercase font-black">
+        Conheça seus direitos
+      </h2>
+      <div className="w-full max-w-md px-4">
+        <Accordion type="single" collapsible>
+          {accordions.map((item, index) => (
+            <AccordionItem value={`item-${index}`} key={index}>
+              <AccordionTrigger className="text-zinc-200 bg-zinc-800 p-4 rounded-md mb-2 hover:bg-zinc-700 font-semibold [&>svg]:text-zinc-200">
+                {item.title}
+              </AccordionTrigger>
+              <AccordionContent className="text-zinc-900 bg-white p-4 rounded-md mb-4">
+                {Array.isArray(item.description) ? (
+                  <ul className="list-disc list-inside space-y-1">
+                    {item.description.map((desc, i) => (
+                      <li key={i} dangerouslySetInnerHTML={{ __html: desc }} />
+                    ))}
+                  </ul>
+                ) : (
+                  <p>{item.description}</p>
+                )}
+              </AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </div>
+      <div className="flex flex-wrap justify-center space-x-4 mt-8 px-4">
+        <Link href="/denuncias/nova" className="mb-4">
+          <button className="text-zinc-900 border-zinc-900 border-2 hover:bg-zinc-900 hover:text-white font-bold py-3 px-6 rounded-full w-full sm:w-auto">
+            Fazer denúncia
+          </button>
+        </Link>
+        <Link href="/historico" className="mb-4">
+          <button className="hover:border-zinc-900 border-2 bg-zinc-900 text-white font-bold py-3 px-6 rounded-full w-full sm:w-auto">
+            Acompanhar denúncias
+          </button>
+        </Link>
+        <Link href="/solicitacoes/nova" className="mb-4">
+          <button className="text-zinc-900 border-zinc-900 border-2 hover:bg-zinc-900 hover:text-white font-bold py-3 px-6 rounded-full w-full sm:w-auto">
+            Solicitar apoio
+          </button>
+        </Link>
+      </div>
+      <div className="mt-12 text-sm text-gray-500 text-center px-4">
+        <p>© 2025 Elos. Todos os direitos reservados.</p>
+        <p>Desenvolvido com apoio do Núcleo Amado.</p>
+      </div>
+    </div>
+  );
 }
